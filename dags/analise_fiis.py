756754a27e6cf139f71979906b7274fc3a0ea69f
adailtonsilva90 <adailton.oliveira90@gmail.com>
2024-06-11 17:17:31 -0300

    correcao do erro de modulo gspread

--- dags/analise_fiis.py
+++ dags/analise_fiis.py
@@ -6,15 +6,16 @@ from airflow.models import Variable
 from airflow.operators.python_operator  import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 #carregando variaveis
 url = Variable.get('url')
 file_path_html = Variable.get('file_path_html')
+file_path_credential = Variable.get('file_path_credential')
 
 dag= DAG('rank_fiis', schedule_interval=None, catchup=False,start_date=datetime(2024, 6 ,4))
 
@@ -32,9 +33,11 @@ def dowload_file_html(url, file_path_html):
     )
 
     try:
+        print("Obtendo dados da url")
         driver.get(url)        
         html_content = driver.page_source
-        
+
+        print("Salvando arquivo html")
         with open(file_path_html, 'w', encoding='utf-8') as file:
             file.write(html_content)
         
@@ -48,7 +51,7 @@ tsk_download_file_html = PythonOperator(task_id='tsk_download_file_html',
                                     dag=dag)
 
 
-def load_table_file_html(file_path_html):
+def load_table_file_html_to_gdrive(file_path_html, file_path_credential):
 
     if os.path.exists(file_path_html):        
         with open(file_path_html, 'r', encoding='utf-8') as file:
@@ -59,18 +62,31 @@ def load_table_file_html(file_path_html):
         tables = soup.find_all('table')
         if tables:
             df = pd.read_html(str(tables),match='Setor')[0]
-            print(df.head(5))
+
+            # Autorize o acesso ao Google Sheets
+            scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+            credentials = ServiceAccountCredentials.from_json_keyfile_name(file_path_credential, scope)
+            client = gspread.authorize(credentials)
+
+            # Encontre a pasta desejada pelo nome
+            pasta = next(folder for folder in client.list_folders() if folder['name'] == 'Airflow')
+            spreadsheet = client.create('rank_fiis', folder_id=pasta['id'])
+            worksheet = spreadsheet.get_worksheet(0)
+            dados = df.values.tolist()
+            worksheet.append_rows(dados)
+            print("DataFrame enviado com sucesso para o Google Sheets na pasta especificada!")
+            
         else:
             print("Tabela não encontrada.")
     else:
         print(f"File {file_path_html} does not exist.")
 
-tsk_load_table_file_html = PythonOperator(task_id='tsk_load_table_file_html', 
-                                        python_callable=load_table_file_html,
-                                        op_args=[file_path_html],
+tsk_load_table_file_html_to_gdrive = PythonOperator(task_id='tsk_load_table_file_html', 
+                                        python_callable=load_table_file_html_to_gdrive,
+                                        op_args=[file_path_html,file_path_credential],
                                         provide_context=True,
                                         dag=dag)
 
 
 
-tsk_download_file_html >> tsk_load_table_file_html
+tsk_download_file_html >> tsk_load_table_file_html_to_gdrive
